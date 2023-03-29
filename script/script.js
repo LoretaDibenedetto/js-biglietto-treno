52c71ca1d96c77f9fc782d430df1e6cca98b24e8
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-03-29 15:28:40 +0200

    fix script

--- script/script.js
+++ script/script.js
@@ -1,29 +1,34 @@
 //prendo i valori dal prompt
-
+const underTextSale = document.getElementById('saleUnderAge');
+const overTextSale = document.getElementById('saleOverAge');
 let userKm = prompt('inserisci i km da percorrere:');
 let age = prompt("inserisci l'eta");
 
 
 let euroKm = userKm * 0.21;
-
+euroKm = parseFloat(euroKm);
 document.getElementById('Prezzo_biglietto').innerHTML = euroKm;
 
+let underSale = euroKm * 0.2;
+let applySale = euroKm - underSale;
 
 
-
+let overSale = euroKm * 0.4;
+let applyOverSale = euroKm - overSale;
+    
 
 if(age < 18){
-    let underSale = euroKm * 0.2;
-    let applySale = euroKm - underSale;
-    document.getElementById(' saleUnderAge').innerHTML = applySale ;
-    console.log(applySale);
     
-}else if(age >= 65){
 
-    let overSale = euroKm * 0.4;
-    let applyOverSale = euroKm - overSale;
+    console.log(applySale);
+    underTextSale.innerText = applySale ;
     
-    document.getElementById('saleOverAge').innerHTML = applyOverSale ;
+}else if(age > 65){
+
     
+    overTextSale.innerHTML = applyOverSale ;
     console.log(applyOverSale);
-};
+    
+}else{
+    console.log('buon viaggio');
+}
